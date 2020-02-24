e2eb87d132db017aeabd4c2826b9ccb27a629946
Anand Pusarla <a.pusarla@gmail.com>
2020-02-24 09:48:43 +0100

    with 1% of training set

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -19,9 +21,21 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 
+clf = svm.SVC(kernel='linear',gamma='auto')
 
+t0 = time()
+clf.fit(features_train,labels_train)
+print("training time:", round(time()-t0, 3), "s")
 
+t1 = time()
+pred = clf.predict(features_test)
+print ("predicting time:", round(time()-t1, 3), "s")
+
+accuracy = accuracy_score(pred,labels_test)
+print(accuracy)
 #########################################################
 ### your code goes here ###
 
